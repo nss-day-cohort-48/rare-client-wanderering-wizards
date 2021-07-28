9c18db8fe2b4f75a14e7d11b0c49d5761da3b728
Andrew Webb <webba@ihopu.org>
2021-07-28 09:24:47 -0500

    displayed all posts

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -12,7 +12,7 @@ export const NavBar = () => {
                 <img className="navbar__logo" src={Logo} />
             </li>
             <li className="navbar__item">
-                <Link className="navbar__link" to="/">Posts</Link>
+                <Link className="navbar__link" to="/posts">All Posts</Link>
             </li>
             <li className="navbar__item">
                 <Link className="navbar__link" to="/myposts">My Posts</Link>
