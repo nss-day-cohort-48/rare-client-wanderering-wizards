a2852fad14ab2fca7003ea55a15842226c93d25f
Devin Garcia <devinmgarcia@gmail.com>
2021-08-17 15:09:13 -0500

    myposts complete

--- src/components/posts/MyPostList.js
+++ src/components/posts/MyPostList.js
@@ -1,70 +1,64 @@
-import React, { useContext, useEffect} from "react"
-import { PostContext } from "./PostProvider"
+import React, { useContext, useEffect } from "react";
+import { PostContext } from "./PostProvider";
 import { Link, useHistory } from "react-router-dom";
-import "./post.css"
+import "./post.css";
 
-export const MyPostList = props => {
-    const {posts, getPostsByUserId} = useContext(PostContext)
-    const currentUserId = parseInt(localStorage.getItem("rare_user_id"))
-    const history = useHistory();
+export const MyPostList = (props) => {
+  const { posts, getPostsByUserId } = useContext(PostContext);
+  const history = useHistory();
 
-    useEffect(() => {
-        getPostsByUserId(currentUserId)
-    }, [])
+  useEffect(() => {
+    getPostsByUserId();
+  }, []);
 
-    const editPostButton = (user_id, post_id) => {
-        if (user_id == localStorage.getItem("rare_user_id")) {
-            return <button
-            className="post blueText"
-            id={`post--${post_id}`}
-            onClick={(event) => {
-              event.preventDefault();
-              handleUpdatePost(post_id);
-            }}
-          >
-            Edit Post
-          </button>
-          }
-          else {
-            return 
-          }
-        }
-
-        const handleUpdatePost = (post_id) => {
-            history.push(`/posts/edit/${post_id}`);
-          };
-
-
-
-    return (
-        <>
+  const editPostButton = (user_id, post_id) => {
+    if (user_id == localStorage.getItem("rare_user_id")) {
+      return (
         <button
+          className="post blueText"
+          id={`post--${post_id}`}
+          onClick={(event) => {
+            event.preventDefault();
+            handleUpdatePost(post_id);
+          }}
+        >
+          Edit Post
+        </button>
+      );
+    } else {
+      return;
+    }
+  };
+
+  const handleUpdatePost = (post_id) => {
+    history.push(`/posts/edit/${post_id}`);
+  };
+
+  return (
+    <>
+      <button
         className="create__button"
         onClick={() => history.push("/Posts/create")}
-        >
+      >
         Create Post?
       </button>
-        <div>
-            <h1>My Posts</h1>
-            {
-                posts.map(post => {
-                    return (
-                        <>
-                        <article className="flex">
-                        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-                        <div>{post.category.label}</div>
-                        <div>{post.user.first_name} {post.user.last_name}</div>
-                        {editPostButton(post.user_id, post.id)}
-                        </article>
-                        </>
-                    )
-                })
-            }
-        </div>
-        </>
-    )
-}
-
-
-
-
+      <div>
+        <h1>My Posts</h1>
+        {posts.map((post) => {
+          return (
+            <>
+              <article className="flex">
+                <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                <div>{post.category.label}</div>
+                <div>
+                  {post.user.first_name} {post.user.last_name}
+                </div>
+                {editPostButton(post.user_id, post.id)}
+              </article>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
